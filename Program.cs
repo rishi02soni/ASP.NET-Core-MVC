344efbe2d68c5ee69d9e6d20226315980f507dd5
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-05-26 23:09:39 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using ExpenseTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(
+builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Expense}/{action=Index}/{id?}");
+
+app.Run();
